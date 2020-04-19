667a1b83b1a437be65ccbedf6f5f7675846b9401
jx-test <rak@vmware.com>
2020-04-19 16:14:07 +0530

    Fixing lintings

--- app.py
+++ app.py
@@ -1,42 +1,31 @@
+"""imports"""
+import logging
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
-import logging
 
 import pandas as pd
 from sklearn.externals import joblib
 from sklearn.preprocessing import StandardScaler
 
-app = Flask(__name__)
-LOG = create_logger(app)
+APP = Flask(__name__)
+LOG = create_logger(APP)
 LOG.setLevel(logging.INFO)
-
-ME = basename(__file__)
-LOGGER = logging.getLogger()
-LOGGER.setLevel(logging.DEBUG)
-# Add the console handler
-handler_console = logging.StreamHandler()
-format_console = logging.Formatter(
-            ME + ": %(asctime)s: %(levelname)8s: %(message)s",
-                datefmt="%Y-%m-%d %H:%M:%S"
-
-        )
-handler_console.setFormatter(format_console)
-LOGGER.addHandler(handler_console)
 
 def scale(payload):
     """Scales Payload"""
 
-    LOG.info("Scaling Payload: \n{}".format(payload))
+    LOG.info(f"Scaling Payload: \n{payload}")
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
 
-@app.route("/")
+@APP.route("/")
 def home():
+    """predictions"""
     html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
-@app.route("/predict", methods=['POST'])
+@APP.route("/predict", methods=['POST'])
 def predict():
     """Performs an sklearn prediction
 
@@ -73,12 +62,12 @@ def predict():
     LOG.info("Inference payload DataFrame: \n{}".format(inference_payload))
     # scale the input
     scaled_payload = scale(inference_payload)
-    # get an output prediction from the pretrained model, clf
-    prediction = list(clf.predict(scaled_payload))
+    # get an output prediction from the pretrained model, CLF
+    prediction = list(CLF.predict(scaled_payload))
     # TO DO:  Log the output prediction value
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
-    # load pretrained model as clf
-    clf = joblib.load("./model_data/boston_housing_prediction.joblib")
-    app.run(host='0.0.0.0', port=8000, debug=True) # specify port=80
+    # load pretrained model as CLF
+    CLF = joblib.load("./model_data/boston_housing_prediction.joblib")
+    APP.run(host='0.0.0.0', port=8000, debug=True) # specify port=80
